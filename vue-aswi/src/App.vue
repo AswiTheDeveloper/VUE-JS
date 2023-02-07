0238e94c3dde2f5abe4c1db0ff072c72c32c97c6
aswi <98392791+DoNotForgetToBeAwesome@users.noreply.github.com>
2023-02-07 07:43:05 +0530

    learned form controls in VUe Js

--- vue-aswi/src/App.vue
+++ vue-aswi/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>{{ name }}</h1>
   <img src="./assets/logo.png" alt="">
-  <HomePage age=23 name="ASWI" />
+  <HomePage age={{ 23 }} name="ASWI" />
   <vueMethods />
   <vueEvents />
+  <vueForm />
 </template>
 
 <script>
 import HomePage from './components/HomePage.vue';
 import vueMethods from './components/vueMethods.vue';
 import vueEvents from './components/vueEvents.vue';
+import vueForm from './components/vueForm.vue';
 export default {
   name: "App",
   data() {
@@ -17,7 +19,7 @@ export default {
       name: 'ASWI'
     };
   },
-  components: { HomePage, vueMethods, vueEvents }
+  components: { HomePage, vueMethods, vueEvents, vueForm }
 }
 </script>
 
